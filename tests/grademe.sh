f2353595fd5902e2d81d1532eec911f80c4125b9
Shogo Kohraku <skohraku@gmail.com>
2020-11-30 14:41:26 +0900

    fix test script

--- tests/grademe.sh
+++ tests/grademe.sh
@@ -1,3 +1,3 @@
 ./testcase.sh > answer.txt
-../minishell < testcase.sh | sed 's/\[31mshell$> \[0m//g' | sed -e "/exit/d" > result.txt
+../minishell < testcase.sh | sed 's/\[32mshell$> \[0m//g' | sed -e "/exit/d" > result.txt
 diff answer.txt result.txt
