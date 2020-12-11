a9ac306354f48518281363bd67c6eb08aba5e954
Toshiki　Nakamura <t.jack0624@gmail.com>
2020-12-11 19:08:46 +0900

    includes env_show.h

--- includes/env_show.h
+++ includes/env_show.h
@@ -1,34 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   env_info.h                                         :+:      :+:    :+:   */
+/*   env_show.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tnakamur <tnakamur@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/12/04 20:39:08 by skohraku          #+#    #+#             */
-/*   Updated: 2020/12/11 18:46:25 by tnakamur         ###   ########.fr       */
+/*   Created: 2020/12/11 18:44:34 by tnakamur          #+#    #+#             */
+/*   Updated: 2020/12/11 18:53:53 by tnakamur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef ENV_INFO_H
-# define ENV_INFO_H
+#ifndef ENV_SHOW_H
+# define ENV_SHOW_H
 
-# define ENV_KEY_QUESTION	"?"
-
-# include "libft.h"
-
-typedef struct	s_env_info
-{
-	char	*key;
-	char	*value;
-}				t_env_info;
-
-extern t_list	*g_env_list_top;
-extern char		*g_env_question;
-extern char		**g_env;
-
-t_env_info		*create_env_info(const char *env);
-void			delete_env_info(void *info);
-int				cmp_env_key(void *info, const char *key);
+void			show_env_list(void);
+void			show_export_list(void);
 
 #endif
