7691474cbb9598f04dcecf364557c89deee6ad2b
Shogo Kohraku <skohraku@gmail.com>
2020-12-12 10:43:54 +0900

    add error output test

--- tests/grademe.sh
+++ tests/grademe.sh
@@ -1,3 +1,10 @@
-/bin/bash ./testcase.sh > answer.txt
-../minishell < testcase.sh | sed 's/\[32mshell$> \[0m//g' | sed -e "/exit/d" > result.txt
+bash testcase.sh > answer.txt 2> answer_error.txt
+../minishell < testcase.sh 2> result_error.txt | sed 's/\[32mshell$> \[0m//g' | sed -e "/exit/d" > result.txt
+echo =================================
+echo ====== Standard Output Test =====
+echo =================================
 diff answer.txt result.txt
+echo =======================================
+echo ====== Standard ERROR Output Test =====
+echo =======================================
+diff answer_error.txt result_error.txt
