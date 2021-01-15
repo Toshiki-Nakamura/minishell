0259669c74bb4995072f1add248f58ad489be19c
Shogo Kohraku <skohraku@gmail.com>
2021-01-15 11:28:29 +0900

    Removed the function to replace ~shogo with home directory

--- srcs/files.c
+++ srcs/files.c
@@ -6,7 +6,7 @@
 /*   By: skohraku <skohraku@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/30 15:17:34 by skohraku          #+#    #+#             */
-/*   Updated: 2020/12/30 16:04:22 by skohraku         ###   ########.fr       */
+/*   Updated: 2021/01/15 11:25:06 by skohraku         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,12 @@
 void       replace_tilde(char **str)
 {
     char	    *p;
-	const char	*user;
 	char		*ret_str;
 
     p = *str;
     if (*p != '~')
         return ;
     p++;
-	user = get_env_value("USER");
-    if (0 == ft_strncmp(p, user, ft_strlen(user)))
-		p += ft_strlen(user);
 	if (!*p || *p == '/')
 	{
 		ret_str = ft_strjoin(get_env_value("HOME"), p);
